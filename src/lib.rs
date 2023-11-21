10b67f7a31900eb83d635cad8b5b492b74e68d31
Andrew Schmid <andrewschmid-dev@proton.me>
2023-11-21 12:57:23 -0600

    Formatted

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
-use axum::{Router, http::StatusCode, routing::get};
+use axum::{http::StatusCode, routing::get, Router};
 
 async fn health_check() -> StatusCode {
     StatusCode::OK
 }
 
 pub fn app() -> Router {
-    Router::new()
-        .route("/health_check", get(health_check))
+    Router::new().route("/health_check", get(health_check))
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,20 @@
-use axum::{http::{Request, StatusCode}, body::{Body, HttpBody}};
-use tower::ServiceExt;
+use axum::{
+    body::{Body, HttpBody},
+    http::{Request, StatusCode},
+};
 use rust_todo::app;
+use tower::ServiceExt;
 
 #[tokio::test]
 async fn health_check_returns_200() {
     let app = app();
     let response = app
-        .oneshot(Request::builder().uri("/health_check").body(Body::empty()).unwrap())
+        .oneshot(
+            Request::builder()
+                .uri("/health_check")
+                .body(Body::empty())
+                .unwrap(),
+        )
         .await
         .unwrap();
 
